28f552f81b06f3d808f7fa85eaa24d4b4944b639
Justin Duong <jduong@umich.edu>
2023-10-16 14:42:34 -0400

    Made changes based on Daniel's suggestions. Added svg icon for closing poll.

--- root/client/src/components/user/dashboard.js
+++ root/client/src/components/user/dashboard.js
@@ -10,9 +10,7 @@ export const Dashboard = () => {
   }
 
   useEffect(() => {
-    const getPolls = async () => {
-      return await fetch('/polls/all').then(data => data.json()).then(result => result.response);
-    }
+    const getPolls = async () => await fetch('/polls/all').then(data => data.json()).then(result => result.response);
 
     try {
       const polls = getPolls();
@@ -40,13 +38,16 @@ export const Dashboard = () => {
       <main>
         <div id="div-main">
           <div id="div-main-header">
-            <h2>Polls</h2>
-            <button onClick={() => { setShowPollForm(!showPollForm)} }>Create</button>
+            <h2>{showPollForm ? 'Create Poll' : 'Polls'}</h2>
+            <button id={showPollForm ? 'btn-poll-create' : 'btn-poll-close'} type='button' onClick={() => { setShowPollForm(!showPollForm)} }>
+              {showPollForm 
+              ? <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+              : 'Create'}
+            </button>
           </div>
         </div>
-        {!showPollForm && <ul id="list-polls">{arrPolls}</ul>}
-        {showPollForm && <PollForm handleSubmit={handleSubmit} />}
+        {showPollForm ? <PollForm handleSubmit={handleSubmit} /> : <ul id="list-polls">{arrPolls}</ul>}
       </main>
     </div>
-  )
-}
+  );
+};
